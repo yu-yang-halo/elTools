a67285b8571e34c1a8f733f5da4b5f9e603b1bca
yuyang <623240480@qq.com>
2015-06-15 12:45:22 +0800

    Signed-off-by: yuyangWinPC <623240480@qq.com>

--- ehomeDebugTools/ehomeDebugTools/res/js/elapi.js
+++ ehomeDebugTools/ehomeDebugTools/res/js/elapi.js
@@ -18,8 +18,9 @@
 // 
 // 
 // })(document,window);
+
+//登录模块
 function login(username,password){
-    //alert("username:"+username+" password:"+password);
     objc_login(username,password);
 }
 function loginIsOk(isOK){
@@ -29,3 +30,90 @@ function loginIsOk(isOK){
         alert('FAIL');
     }
 }
+//设备列表模块
+function hyl_requestDevicesCmd(){
+	objc_requestDevices();
+}
+function hyl_loadDevicesData(devices){
+	/*格式【｛objectId ｝，｛｝，｛｝  】
+	
+	[
+	{   objectId:1109,
+		classId:1,
+		netstate:1,
+		cmdField:
+		    {fieldId:110,
+			fieldValue:1},
+		statusField:
+		[
+		{
+		 fieldId:112,
+		 fieldValue:0}
+		]
+	},
+	{
+		
+	}
+	]
+	
+	
+	
+	*/
+	if(devices!=undefined&&devices.length>0){
+		for(var i=0;i<devices.length;i++){
+			var obj=devices[i];
+			var trString="";
+			trString+="<tr objId="+obj.objectId+">";
+			trString+="<td><img src='img/device_gas.png'></img></td>";
+			
+			trString+="<td><div class='display'>"+
+			"<label class='name'>"+obj.name+"</label>"+
+			"<img class='onlineImg' src='img/icon_online.png'></img>"+
+			"<label class='onlineText'>在线</label>"+
+			"</div></td>";
+			
+			trString+="<td><input type='button' class='btn'></input></td></tr>";
+			
+			$("#deviceTable").append(trString);
+			
+			
+		}
+		
+	}else{
+		
+		$("#deviceTable").append("数据为空");
+		
+	}
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
