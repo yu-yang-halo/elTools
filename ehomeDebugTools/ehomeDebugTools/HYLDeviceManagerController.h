9cba4dd7fb2e3dee46bf9b49a2e74b4f263be0f5
yuyang <623240480@qq.com>
2015-06-29 09:50:50 +0800

    资源数据添加

---
+++ ehomeDebugTools/ehomeDebugTools/HYLDeviceManagerController.h
@@ -0,0 +1,13 @@
+//
+//  HYLDeviceManagerController.h
+//  ehomeDebugTools
+//
+//  Created by admin on 15/6/26.
+//  Copyright (c) 2015年 cn.lztech  &#21512;&#32933;&#32852;&#27491;&#30005;&#23376;&#31185;&#25216;&#26377;&#38480;&#20844;&#21496;. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HYLDeviceManagerController : UIViewController<UIScrollViewDelegate>
+
+@end
---
+++ ehomeDebugTools/ehomeDebugTools/HYLDeviceManagerController.m
@@ -0,0 +1,51 @@
+//
+//  HYLDeviceManagerController.m
+//  ehomeDebugTools
+//
+//  Created by admin on 15/6/26.
+//  Copyright (c) 2015年 cn.lztech  &#21512;&#32933;&#32852;&#27491;&#30005;&#23376;&#31185;&#25216;&#26377;&#38480;&#20844;&#21496;. All rights reserved.
+//
+
+#import "HYLDeviceManagerController.h"
+#import <JavaScriptCore/JavaScriptCore.h>
+@interface HYLDeviceManagerController ()
+@property (strong, nonatomic) IBOutlet UIWebView *webView;
+- (IBAction)switchPage:(id)sender;
+
+@end
+
+@implementation HYLDeviceManagerController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    [self.webView.scrollView setShowsHorizontalScrollIndicator:NO];
+    [self.webView.scrollView setShowsVerticalScrollIndicator:NO];
+    self.webView.scrollView.delegate=self;
+    
+    NSString *htmlString=[NSString stringWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"manager.html" ofType:@""] encoding:NSUTF8StringEncoding error:nil];
+    
+    [self.webView loadHTMLString:htmlString baseURL:[[NSBundle mainBundle] bundleURL]];
+    JSContext *context=[self.webView valueForKeyPath:@"documentView.webView.mainFrame.javaScriptContext"];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)switchPage:(id)sender {
+    
+}
+@end
